2cd8243f9519a141dbf8e4d8a6973967a584416c
Michael <michaelem3@gmail.com>
2015-07-29 15:25:15 -0400

    Basic authentication working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+    if @user
+      #logged in, hooray
+      session[:user_id] = @user.id #put the current user in the session hash
+      redirect_to notes_path
+    else
+      render action: 'new'
+    end
+  end
 end
