77999a58c15636c0eb7076a9bc1c457064444bb4
Michael <michaelem3@gmail.com>
2015-07-31 15:15:12 -0400

    Change to application_controller

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,6 @@
 class ClubsController < ApplicationController
+  before_action :set_club, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:new]
   def new
     @club = Club.new
   end
@@ -9,10 +11,13 @@ class ClubsController < ApplicationController
 
   def create
     @club = Club.new(club_params)
+
     if @club.save
+      UserClub.create(club:@club, user: current_user)
       redirect_to club_path(@club)
     else
       flash[:error] = 'An error occured!'
+      redirect_to new_club_path
     end
   end
 
@@ -21,6 +26,10 @@ class ClubsController < ApplicationController
   end
 
   private
+  def set_club
+    @club = Club.find(params[:id])
+  end
+  
   def club_params
     params.require(:club).permit(:title, :description, :image, :location, :tag_list)
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end 
+
   def causes
   end
 end
