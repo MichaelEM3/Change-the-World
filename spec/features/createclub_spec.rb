95039031045d01923862bd6acd375bbb15d8fd23
Prahasi Kacham <pk285@nova.edu>
2015-08-04 19:49:56 -0400

    changes to spec file

--- spec/features/createclub_spec.rb
+++ spec/features/createclub_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a club' do
   before(:each) do
-      visit ('/signup')
+      visit('/signup')
         fill_in('Username', with: 'Tester10')
         fill_in('Name', with: 'Test')
         fill_in('Email', with: 'test@test.com')
@@ -10,13 +10,7 @@ RSpec.describe 'Creating a club' do
       click_button('Create User')
   end
   it 'should be at clubs/new' do
-      visit '/clubs/new'
+      visit('/clubs/new')
       expect(current_path).to be == '/login'
   end
-end
-
-it 'should be at clubs/new' do
-  visit '/clubs/new'
-    expect(current_path).to be == '/login'
-  end
 end
