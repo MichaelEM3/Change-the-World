febac3114cb8ecdaca553863082110c279c54a2f
Michael <michaelem3@gmail.com>
2015-08-06 12:20:16 -0400

    Removed unnecessary models and began styling

--- db/migrate/20150730010833_add_column_to_tag_clubs.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToTagClubs < ActiveRecord::Migration
-  def change
-    add_reference :tag_clubs, :tag, index: true, foreign_key: true
-    add_reference :tag_clubs, :club, index: true, foreign_key: true
-  end
-end
--- test/models/tag_club_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TagClubTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
