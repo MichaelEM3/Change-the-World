a8486b48ebf3b98e6efe749cc5797180b6ff6393
Anders Palm <ang.palm@hotmail.com>
2015-08-10 16:56:08 -0400

    deleted uneccessary text in css and javascript files

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,13 +1,2 @@
 
 
-$(document).ready(function(){
-  $('#new_commentary').on('submit', function(evt){
-    evt.preventDefault();
-    $.ajax('#commentary_comment', {
-      success: function(evt, data) {
-        alert("working");
-        $(this).appendTo(".story_comments");
-      }
-    });
-  });
-});
