9e3d488742522a74ae16a2e0dd20fc239868149a
Michael <michaelem3@gmail.com>
2015-08-10 23:18:20 -0400

    Fixed login and signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 // // $(ready);
 // $(document).on('page:load', ready);
 // $('.user_club_show').hide();
-//  $('.stories_show').hide(); 
+//  $('.stories_show').hide();
 //   $('.about_container').hide()
 // });
 
@@ -32,40 +32,40 @@ $(document).ready(function(){
 
           $('.user_club_show').slideToggle();
 
-      });     
+      });
 
      $('.club_stories').on('click', function(){
       console.log("This")
 
           $('.stories_show').slideToggle();
 
-      });    
-    
+      });
+
       $('.club_details').on('page:load', function(){
 
-          $('.about_container').slideToggle();  
+          $('.about_container').slideToggle();
 
       });
 
-      
+
 
      // $('.leave_a_comment').on('click', function(){
-     //      $(this).sibling('story_comments').slideToggle();            
+     //      $(this).sibling('story_comments').slideToggle();
      //  });
 
        $('.leave_a_comment').on('page:load', function(){
 
           $(this).next().slideToggle();
 
-      });   
+      });
 
       //  $('.leave_a_comment').on('click', function(){
-      //     $('.story_comments').slideToggle();            
-      // });  
+      //     $('.story_comments').slideToggle();
+      // });
 
       // $('.leave_a_comment').on('click', function(){
-      //     $('.story_comments').slideToggle();            
-      // });  
+      //     $('.story_comments').slideToggle();
+      // });
 
       $('.toggle_live_chat').on('click', function(){
       	$('live_chat_container').slideToggle();
@@ -79,5 +79,20 @@ $('.leave_a_comment').on('click', function(){
 
           $(this).next().slideToggle();
 
-      });   
-  });  
+      });
+  });
+
+  // Signup and login
+  function ready() {
+    $('.trigger').on("click", function() {
+      $('.overlay').show();
+      $('.modal').show('slow');
+    });
+
+    $('.overlay').on("click", function() {
+      $(this).hide();
+      $('.modal').hide('slow');
+    });
+  };
+
+  $(document).on('ready page:load', ready)
